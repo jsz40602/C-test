755d59c47b3c68f49e8f02dfc61b15d6a3022de5
jsz40602 <jsz82513@gmail.com>
2018-01-25 14:15:34 +0800

    第一次練習

---
+++ Lecture01-Example/Example1-Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1_Console
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("請輸入名字:");
+            string name = Console.ReadLine();
+            Console.Write("今天心情如何?");
+            string status = Console.ReadLine();
+            Console.WriteLine("我叫{0},今天心情{1}", name, status);
+        }
+    }
+}
