72ca88a07cb065a92bd02a5740d3358fe557df88
jsz40602 <jsz82513@gmail.com>
2018-02-23 16:09:08 +0800

    第三次練習

---
+++ PokemonLibrary/PokemonLibrary/Pokemon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonLibrary
+{
+    public class Pokemon
+    {
+        public string Name;
+        public string NationalNo;
+        public string[] Type;
+        public string Species;
+        public double Height;
+        public double Weight;
+    }
+}
---
+++ PokemonLibrary/TestApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PokemonLibrary;
+
+namespace TestApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Pokemon pokemon = new Pokemon()
+            {
+                Name = "Bulbasaur",
+                NationalNo = "001",
+                Type = ["Grass", "Poison"],
+                Species = "Seed",
+                Height = 0.71,
+                Weight = 6.9
+            };
+        }
+    }
+}
